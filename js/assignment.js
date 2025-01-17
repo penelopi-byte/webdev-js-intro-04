43f63cfcbd50decb8a70424b1cba3a07d0b2c9d5
penelopi-byte <Ninnydhynes@gmail.com>
2025-01-16 23:27:07 -0500

    Completed part one and two, making the age reader work.

--- js/assignment.js
+++ js/assignment.js
@@ -3,15 +3,23 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
-
+const responseParagraph = document.getElementById('response');
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
-    // Write your code below
 
+    if (isNaN(age)) {
+        responseParagraph.textContent = "Please provide a valid age.";
+    } else if (age >= 18 && age < 21) {
+        responseParagraph.textContent = "You can vote, but you cannot purchase alcohol.";
+    } else if (age >= 21) {
+        responseParagraph.textContent = "You can vote and purchase alcohol.";
+    } else {
+        responseParagraph.textContent = "You cannot vote and you cannot purchase alcohol.";
+    }
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let healthPoints = 5;
+
+if (healthPoints >= 8) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+
+
+    const number = -5;
+
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+const isLoggedIn = false;
+
+if (isLoggedIn) {
+    console.log("Welcome back, user!");
+} else {
+    console.log("Please log in to continue.");
+}
+
+
+const age = 45;
+
+if (age >= 60) {
+    console.log("You are a senior citizen.");
+} else if (age >= 30) {
+    console.log("You are an adult.");
+} else if (age >= 18) {
+    console.log("You are a young adult.");
+} else {
+    console.log("You are a teenager or child.");
+}
+    */
